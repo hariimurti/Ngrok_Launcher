d058132e3ab9c818e95f5ee31275fabb33d549b3
Harimurti Wibowo <hariimurti@gmail.com>
2017-09-09 23:25:01 +0700

    some info

--- Ngrok_Launcher/Properties/AssemblyInfo.cs
+++ Ngrok_Launcher/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ngrok_Launcher")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Ngrok Launcher")]
+[assembly: AssemblyDescription("Ngrok – Secure tunnels to localhost")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ngrok_Launcher")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("Harimurti.net")]
+[assembly: AssemblyProduct("Ngrok Launcher")]
+[assembly: AssemblyCopyright("Copyright © 2017 Harimurti.net")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0")]
